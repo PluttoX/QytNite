95ad1531f229472f35613d4568d2358c20ebddf1
Malik Ahmad Mukhtar <malikahmadmukhtar165@gmail.com>
2024-05-27 21:35:41 +0500

    added class and method for google sign-in using firebase

---
+++ lib/services/google_signin_controller.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleSignInController {
+  // method to sign-in user with google
+  Future<dynamic> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      return await FirebaseAuth.instance.signInWithCredential(
+          credential); //returning the credentials of the signed-in user after signing-in with the credentials
+    } on Exception catch (e) {
+      return e; //returns an exception if occurred
+    }
+  }
+}
